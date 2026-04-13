d75017d195e9bb72c97e47aab56e04f129c128c5
BigFoots625 <ranggatimotius@gmail.com>
2026-04-13 18:37:51 +0700

    fix dockerfile SSL path

--- login-app/ssl/generate-cert.sh
+++ login-app/ssl/generate-cert.sh
@@ -4,8 +4,8 @@ echo "🔐 Generating self-signed SSL certificate..."
 
 openssl req -x509 \
   -newkey rsa:4096 \
-  -keyout /ssl/server.key \
-  -out /ssl/server.cert \
+  -keyout /app/ssl/server.key \
+  -out /app/ssl/server.cert \
   -days 365 \
   -nodes \
   -subj "/C=ID/ST=Jakarta/L=Jakarta/O=SecureApp-101032300137/OU=IT/CN=localhost" \
